61e468c684081e0c38d4f5df675493a8f00dd9ff
ds4113-crypto <ds4113@srmist.edu.in>
2026-03-24 10:57:15 +0530

    Usecase9

---
+++ App/src/ReservationValidator.java
@@ -0,0 +1,21 @@
+public class ReservationValidator {
+
+    public void validate(
+            String guestName,
+            String roomType,
+            RoomInventory inventory
+    ) throws InvalidBookingException {
+
+        if (guestName == null || guestName.trim().isEmpty()) {
+            throw new InvalidBookingException("Guest name cannot be empty.");
+        }
+
+        if (!inventory.isValidRoomType(roomType)) {
+            throw new InvalidBookingException("Invalid room type selected.");
+        }
+
+        if (inventory.getAvailability(roomType) <= 0) {
+            throw new InvalidBookingException("Selected room is not available.");
+        }
+    }
+}
