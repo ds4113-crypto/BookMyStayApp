911a94dd54e46708fbf2bb2cea5731daa692ccd6
ds4113-crypto <ds4113@srmist.edu.in>
2026-03-24 10:10:55 +0530

    Usecase1

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,10 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("Welcome to the Hotel Booking Management System");
+
+        // Initialization message
+        System.out.println("System initialized successfully.");
+    }
+}
